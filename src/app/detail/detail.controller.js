7066ac85af66a293f22e150188bda2fda9e15eaf
Sébastien BEAU <sebastien.beau@akretion.com>
2015-03-02 20:28:20 +0100

    [REF] use object instead of a list for storing the moves

--- src/app/detail/detail.controller.js
+++ src/app/detail/detail.controller.js
@@ -4,23 +4,24 @@ angular.module('pickadoo')
     .controller('DetailCtrl', function( $rootScope, $scope, $stateParams, $state ) {
         $scope.item = $rootScope.items[$stateParams.id]
         $scope.todoMoves = $rootScope.items[$stateParams.id].moves
-        $scope.processMoves = []
+        $scope.processMoves = {}
 
         $scope.add_all = function() {
             $scope.processMoves = $scope.todoMoves;
-            $scope.todoMoves = []
+            $scope.todoMoves = {}
         };
-        $scope.move_add_all = function( index ) {
-            $scope.processMoves.push($scope.todoMoves[index]);
-            $scope.todoMoves.splice(index, 1);
+        $scope.move_add_all = function( id ) {
+            $scope.processMoves[id] = $scope.todoMoves[id];
+            delete $scope.todoMoves[id];
+            console.log($scope.todoMoves);
         };
-        $scope.add_all = function( index ) {
+        $scope.move_add_one = function( id ) {
             $scope.processMoves = $scope.todoMoves;
-            $scope.todoMoves = []
+            $scope.todoMoves = {}
         };
-        $scope.move_cancel_process = function( index ) {
-            $scope.todoMoves.push($scope.processMoves[index]);
-            $scope.processMoves.splice(index, 1);
+        $scope.move_cancel_process = function( id ) {
+            $scope.todoMoves[id] = $scope.processMoves[id];
+            delete $scope.processMoves[id];
         };
 
         $scope.cancel = function() {
