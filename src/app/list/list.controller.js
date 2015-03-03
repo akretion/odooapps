f8306f78a1890fd44a880005f095f241f3dc0c54
Sébastien BEAU <sebastien.beau@akretion.com>
2015-03-03 12:53:26 +0100

    [IMP] only go to detail page if picking is paid (call to paybox will be done at this moment)

--- src/app/list/list.controller.js
+++ src/app/list/list.controller.js
@@ -1,8 +1,17 @@
 'use strict';
 
 angular.module('pickadoo')
-    .controller('ListCtrl', function( $rootScope, $scope, $state ) {
+    .controller('ListCtrl', function( $rootScope, $scope, $state, jsonRpc ) {
         $scope.open = function (item) {
-            $state.go('detail', {id:item.id});
+            // We have to add a blocking call as CB will be captured
+            // Before processing the expedition
+            jsonRpc.call('stock.picking.out', 'start_processing', [[item.id]], {})
+                .done(function(){
+                    $state.go('detail', {id:item.id});
+                })
+                .fail(function() {
+                    $state.go('list');
+                    }
+            )
         };
     });
