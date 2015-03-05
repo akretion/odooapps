69d43efe5edb9b6491f0817ef147fa3ae3dac75e
FranzP <francois.poizat@gmail.com>
2015-03-05 16:24:21 +0100

    added error handling

--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -1,10 +1,16 @@
 'use strict';
 angular.module('pickadoo')
     .controller('LoginCtrl', function ($rootScope, $scope, jsonRpc, $state) {
-        $scope.login = function (loginForm) {
-            jsonRpc.login('db', loginForm.loginInput.$modelValue, loginForm.passwordInput.$modelValue)
+        $scope.login = function () {
+            jsonRpc.login('db', $scope.bucheUsername, $scope.buchePassword)
                 .then(function(data) {
                     $state.go('list');
-                })
+                }, function(reason) {
+                  $scope.bucheLoginForm.result = {
+                    $error: {
+                      credentials = true
+                    }
+                  }
+                });
         };
     });
