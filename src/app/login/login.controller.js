2aa63611a63c7fad72bedba879e3c0961775f357
Sébastien BEAU <sebastien.beau@akretion.com>
2015-03-09 14:30:02 +0100

    [FIX] fix raising error

--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -2,13 +2,13 @@
 angular.module('pickadoo')
     .controller('LoginCtrl', function ($rootScope, $scope, jsonRpc, $state) {
         $scope.login = function () {
-            jsonRpc.login('db', $scope.bucheUsername, $scope.buchePassword)
+            jsonRpc.login('test_2014_09_10_bis', $scope.bucheUsername, $scope.buchePassword)
                 .then(function(data) {
                     $state.go('list');
                 }, function(reason) {
                   $scope.bucheLoginForm.result = {
                     $error: {
-                      credentials = true
+                      credentials: true
                     }
                   }
                 });
