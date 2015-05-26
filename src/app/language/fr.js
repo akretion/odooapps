ceee64c9c941a5abd99bdd62dc14ea45cb0aed8e
Sébastien BEAU <sebastien.beau@akretion.com>
2015-05-26 11:33:29 +0200

    [IMP] remove partial expedition as this should be decide on backend side

--- src/app/language/fr.js
+++ src/app/language/fr.js
@@ -1,3 +1,5 @@
+'use strict';
+
 window.translateFR = {
     SEARCH: 'Recherche',
     ORIGIN: 'Commande',
@@ -11,7 +13,6 @@ window.translateFR = {
     DONE: 'Traité',
     ADD_ONE: '+ 1',
     CANCEL: 'Annuler',
-    PARTIAL: 'Envoie Partiel',
     NO_ORDER_FOUND: 'Aucune commande trouvé, veuillez resaisir le numéro',
     USER_ERROR: 'Erreur utilisateur',
     NO_PRODUCT_FOUND: 'Le code barre saisi ne correspond pas à un produit de la commande',
@@ -26,6 +27,5 @@ window.translateFR = {
     SET_CARRIER_ORDER_AS_PREPARED: 'Le transporteur sélectionné ne peut pas être traité via l\'application. Veuillez mettre la commande dans le bac des {{carrier_name}}',
     SET_PAID_ORDER_AS_PREPARED: 'La commande n\'est pas payé. Veuillez la mettre dans le bac des commandes préparés',
     PARTIAL: 'Partiel'
- 
 }
 
