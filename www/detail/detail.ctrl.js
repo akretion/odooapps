0a4e93ff9dd49ce6e457e879a33fba6a9f539f0a
Sébastien BEAU <sebastien.beau@akretion.com>
2015-06-04 03:24:44 +0200

    [STEP 6] add confirmation

--- www/detail/detail.ctrl.js
+++ www/detail/detail.ctrl.js
@@ -1,5 +1,13 @@
 'use strict';
 angular.module('starter')
-    .controller('DetailCtrl', ['$scope', '$stateParams', 'jsonRpc', 'production', function ($scope, $stateParams, jsonRpc, production) {
+    .controller('DetailCtrl', ['$scope', '$stateParams', 'jsonRpc', '$state', 'production', function ($scope, $stateParams, jsonRpc, $state, production) {
         $scope.item = production.data[$stateParams.id];
+        $scope.confirm = function() {
+            jsonRpc.call('mrp.production', 'prodoo_produce', [$scope.item.id], {})
+                .then(function() {
+                    delete production.data[$scope.item.id];
+                    $state.go('list');
+                }
+            )
+        }
 }]);
