e0bfc0920542a4e25141af7b25611122dcea54cb
Sébastien BEAU <sebastien.beau@akretion.com>
2017-06-13 16:02:20 +0200

    [FIX] fix cn23 printing

--- pickadoo_colissimo/stock.py
+++ pickadoo_colissimo/stock.py
@@ -1,47 +1,46 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
-#
-#   Module for OpenERP
 #   Copyright (C) 2014 Akretion (http://www.akretion.com).
 #   @author Sébastien BEAU <sebastien.beau@akretion.com>
-#
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU Affero General Public License as
-#   published by the Free Software Foundation, either version 3 of the
-#   License, or (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU Affero General Public License for more details.
-#
-#   You should have received a copy of the GNU Affero General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+#   AGPL
 ###############################################################################
 
-from openerp.osv import fields, orm
+from openerp.osv import orm
 from openerp import netsvc
 
 
 class StockPickingOut(orm.Model):
-    _inherit="stock.picking.out"
+    _inherit = "stock.picking.out"
 
     def _prepare_todo_print_list(self, cr, uid, ids, context=None):
         todo = super(StockPickingOut, self)._prepare_todo_print_list(
             cr, uid, ids, context=context)
         picking = self.browse(cr, uid, ids, context=context)[0]
+        proxy_obj = self.pool['proxy.action.helper']
+        # old cn23
         if picking.colipostefr_send_douane_doc:
-            proxy_obj = self.pool['proxy.action.helper']
-            #CN23 are printing in first
-            todo.insert(0,
-                proxy_obj.get_print_report_action(
-                    cr, uid, 'report.stock.picking.cn23',
-                    'stock.picking.out', ids,
-                    copies=3,
-                    context=context)
-                )
-            #Add invoice report
+            # CN23 are printing in first
+            todo.insert(0, proxy_obj.get_print_report_action(
+                cr, uid, 'report.stock.picking.cn23',
+                'stock.picking.out', ids,
+                copies=3,
+                context=context))
+            print '   >>>> Send Douane CN23'
+
+        # New WS CN23
+        cn_obj = self.pool['ir.attachment']
+        cn_ids = cn_obj.search(cr, uid, [
+            ['res_id', 'in', ids],
+            ['name', 'ilike', 'CN23%.pdf'],
+        ], context=context)
+        if cn_ids:
+            cn = cn_obj.browse(cr, uid, cn_ids, context=context)[0]
+            todo.insert(0, proxy_obj.get_print_data_action(
+                cr, uid, cn.datas, raw=True, context=context))
+            print '   >>>> CN23 in attachment'
+
+        # Add invoice report
+        if (cn_ids or picking.colipostefr_send_douane_doc) and picking.sale_id:
             invoice_ids = []
             for invoice in picking.sale_id.invoice_ids:
                 invoice_ids.append(invoice.id)
@@ -50,10 +49,10 @@ class StockPickingOut(orm.Model):
                     wf_service.trg_validate(
                         uid, 'account.invoice',
                         invoice.id, 'invoice_open', cr)
-            todo.insert(1,
-                proxy_obj.get_print_report_action(
-                    cr, uid, 'report.account.invoice',
-                    'account.invoice', invoice_ids,
-                    context=context)
-                )
+            todo.insert(1, proxy_obj.get_print_report_action(
+                cr, uid, 'report.account.invoice',
+                'account.invoice', invoice_ids,
+                context=context))
+            print '   >>>> Facture'
+
         return todo
