dea836006dbd3dfbbfc6873250e289833acd0fac
Florian da Costa <florian.dacosta@akretion.com>
2017-11-24 02:21:40 +0100

    Add telium helper and improve javascript flexibility

--- proxy_action/helper.py
+++ proxy_action/helper.py
@@ -2,8 +2,16 @@
 # Copyright 2014 Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, models
+from openerp import api, models, exceptions, _
 import base64
+import simplejson
+
+TELIUM_PAYMENT_TYPE_MAPPING = {
+    'debit': '0',
+    'credit': '1',
+    'cancel': 2,
+    'pre_authorization': 4,
+}
 
 
 class ProxyActionHelper(models.AbstractModel):
@@ -31,13 +39,14 @@ class ProxyActionHelper(models.AbstractModel):
             'params': {
                 'args': [printer_name, data],
                 'kwargs': kwargs,
-                }
+                },
+            'type': 'POST',
             }
 
     def get_print_report_action(self, records, report_name, **kwargs):
         data = self.env['report'].get_pdf(records.ids, report_name)
         data = base64.b64encode(data)
-	return self.get_print_data_action(data, **kwargs)
+        return self.get_print_data_action(data, **kwargs)
 
     def send_proxy(self, todo):
         """ @param todo: list of requests
@@ -47,3 +56,50 @@ class ProxyActionHelper(models.AbstractModel):
             'type': 'ir.actions.act_proxy',
             'action_list': todo,
             }
+
+    @api.model
+    def display_failure_message(self, result):
+        raise exceptions.Warning(
+            _('Proxy action have failed, check if pywebdriver is running or '
+              'if the hardware is connected to the computer'))
+
+    @api.model
+    def get_telium_data_action(self, amount, payment_mode='card',
+                               payment_type='debit', currency='EUR',
+                               delay=False, delay_timeout=120,
+                               auto=False, host='https://localhost',
+                               return_method=False, return_model=False):
+        """
+            if delay is true, the terminal answer will be at the end of the
+            transactions, else, it will be immediatly after the payment
+            terminal received the request
+
+            if delay is True, we must set a hight timeout to let time to
+            the customer to make the transaction
+
+            if auto is true, the authorization request will be forced.
+        """
+        payment_info = {
+            'amount': amount,
+            'payment_mode': payment_mode,
+            'payment_type': TELIUM_PAYMENT_TYPE_MAPPING.get('payment_type'),
+            'currency_iso': currency,
+            'auto': auto and 'B011' or 'B010',
+            'delay': delay and 'A010' or 'A011',
+        }
+        if delay:
+            payment_info = delay_timeout
+        url = ('%s/hw_proxy/payment_terminal_transaction_start_with_answer'
+               % host)
+        return {
+            'url': url,
+            'params': {
+                'params': {
+                    'payment_info': simplejson.dumps(payment_info)
+                }
+            },
+            'type': 'POST',
+            'block_ui': True,
+            'return_method': return_method,
+            'return_model': return_model,
+        }
--- proxy_action/static/src/js/proxy_view.js
+++ proxy_action/static/src/js/proxy_view.js
@@ -2,17 +2,39 @@ openerp.proxy_action = function (instance) {
     instance.web.ActionManager.include({
         ir_actions_act_proxy: function (action, options) {
             action.action_list.forEach(function (task) {
+                if (task['block_ui']) {
+                    openerp.web.blockUI()
+                }
                 $.ajax({
                     url: task['url'],
-                    type: 'POST',
+                    type: task['type'],
                     data: JSON.stringify(task['params']),
                     contentType: 'application/json',
                 }).done(function (result) {
-                    console.log("Proxy action have been done with sucess", result);
+                    console.log("Proxy action have been done with sucess", result, task['return_method']);
+                    if (task['return_method'] && task['return_model']) {
+                        openerp.session.rpc('/web/dataset/call_kw/' + task['return_model'] + '/' + task['return_method'],
+                            {model: task['return_model'],
+                             method: task['return_method'],
+                             args:[result],
+                             kwargs:{}})
+                    }
+                    if (task['block_ui']) {
+                        openerp.web.unblockUI()
+                    }
+                    openerp.web.unblockUI()
                     //TODO add an UI feedback
                 }).fail(function (result) {
                     console.log('Proxy action have failed', result);
-                    //TODO add an UI feedback
+                    if (task['block_ui']) {
+                        openerp.web.unblockUI()
+                    }
+                    // Call Odoo instead of using alert so we are more flexible
+                    openerp.session.rpc('/web/dataset/call_kw/proxy.action.helper/display_failure_message',
+                        {model: 'proxy.action.helper',
+                         method: 'display_failure_message',
+                         args:[result],
+                         kwargs:{}})
                 })
             })
             this.do_action({"type":"ir.actions.act_window_close"});
